10e8f992d63e612e2daed4200c4aeaf8d9698d94
Ivan Gavryliuk <ivan.gavryliuk@outlook.com>
2017-12-13 16:24:13 +0000

    add some validation logic

---
+++ src/MarkdownTool/IParameters.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarkdownTool
+{
+   public interface IParameters
+   {
+      string InputPath { get; }
+
+      string OutputDir { get; }
+   }
+}
--- src/MarkdownTool/Program.cs
+++ src/MarkdownTool/Program.cs
@@ -1,12 +1,49 @@
-﻿using System;
+﻿using Config.Net;
+using LogMagic;
+using NetBox.Extensions;
+using System;
+using System.IO;
 
 namespace MarkdownTool
 {
    class Program
    {
-      static void Main(string[] args)
+      private static readonly LogMagic.ILog log = LogMagic.L.G(typeof(Program));
+
+      static int Main(string[] args)
       {
-         Console.WriteLine("Hello World!");
+         IParameters p = new ConfigurationBuilder<IParameters>()
+            .UseCommandLineArgs(
+               nameof(IParameters.InputPath).PairedWith(1),
+               nameof(IParameters.OutputDir).PairedWith(2)
+            )
+            .Build();
+
+         L.Config.WriteTo.PoshConsole("{message}{error}");
+
+         log.Trace(".NET Markdown CLI v{0}", typeof(Program).FileVersion());
+
+         int r = Validate(p);
+         if (r != 0) return r;
+
+         return 0;
+      }
+
+      private static int Validate(IParameters p)
+      {
+         if(p.InputPath == null)
+         {
+            log.Trace("input path is missing");
+            return 1;
+         }
+
+         if(!File.Exists(p.InputPath))
+         {
+            log.Trace("input file {0} does not exist");
+            return 2;
+         }
+
+         return 0;
       }
    }
 }
